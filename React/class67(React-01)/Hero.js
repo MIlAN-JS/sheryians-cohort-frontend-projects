30182aba0389c5a7ca8ed15abaf8f6dafdaa3915
MIlAN-JS <milantamang350359@gmail.com>
2025-12-18 10:46:49 +0545

    new c-67 file created with change in file name

---
+++ React/class67(React-01)/Hero.js
@@ -0,0 +1,52 @@
+  function companyDets(){
+
+     let projH1 = React.createElement("h1", {className : "proj-h1"}, "+200");
+     let projP = React.createElement("p", {className:"proj-p"}, "Project completed");
+     let startupH1 = React.createElement("h1", {className : "startup-h1"}, "+50");
+     let startupP = React.createElement("p", {className:"startup-p"}, "Startup raised");
+
+     let completedProj = React.createElement("div", {className:"completed-proj"}, [projH1 , projP])
+     let startupRaised = React.createElement("div", {className:"startup-raised"}, [startupH1, ,startupP])
+
+   let companyDetsContainer;
+     return  companyDetsContainer = React.createElement("div"  , {className :"dets-container"}, [completedProj,startupRaised])
+
+
+     
+
+    
+}
+
+function HeroText(){
+     let heroH1 = React.createElement("h1",{className: "hero-h1"}, "Hello")
+     let heroP = React.createElement("p",{className: "hero-h1"}, "-It's D.Nova a design wizerd")
+
+
+
+     let heroTxtContainer = React.createElement("div", {className : 'hero-txt-container'}, [heroH1, heroP])
+     return heroTxtContainer
+}
+
+function HeroBtn(){
+     
+     let heroBtn = React.createElement("button", {className:'hero-btn'}, "Scroll down ↓")
+     return heroBtn
+}
+
+
+export default function Hero(){
+
+
+     let modelImg = React.createElement("img",{src:"./model0-r0.png"})
+     console.log(modelImg)
+     let heroContainer = React.createElement("div", {className: "hero-container"}, [companyDets
+          (),HeroText(), HeroBtn(),modelImg
+     ])
+
+     return heroContainer;
+
+
+
+         
+}
+
---
+++ React/class67(React-01)/script.js
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar.js";
+import Hero from "./Hero.js";
+
+
+function render(){
+    let root = ReactDOM.createRoot(document.querySelector("#root"))
+    let mainContainer = React.createElement("div" , null , [Navbar(), Hero()])
+    root.render(mainContainer)
+}
+
+render()
